3399df3f5ff2a7ed8a023fd85ee967d43cfef40a
Saehanseul <saehanseul@gmail.com>
2024-02-20 16:14:19 +0900

    Feat: rn master 2.3까지 봄

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
+
+function App() {
+  return (
+    <Father>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
+  );
+}
+
+export default App;
